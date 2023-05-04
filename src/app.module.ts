019e9e53175fa52baf49d589e3e75602f5fed41e
Cristhian García Vélez <cristhian.garcia@momentu.co>
2023-05-04 12:57:37 -0500

    add Database module and Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigurationModule } from './config/configuration.module';
+import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigurationModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
